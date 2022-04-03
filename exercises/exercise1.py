6f75cdb6758445eff8541abb9bf2d60726038faa
Maximiliano Soria <maxi_soria@outlook.com>
2022-04-03 03:45:47 -0300

    cambie cuatro ej

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -5,11 +5,12 @@
 Calcular el área del cuadrado usando las variables disponibles.
 Restricción: Usar el operador de multiplicación
 """
-
+cantidad_a_comprar = 2
 lado_cuadrado = 5
+es_divisible_por_siete = 7
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado * lado_cuadrado
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -22,7 +23,7 @@ Re-Escribir usando el operador de potencia.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado ** lado_cuadrado 
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -35,7 +36,7 @@ Re-Escribir usando la función pow.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+pow(area_cuadrado,lado_cuadrado)
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -50,7 +51,7 @@ precio = 3.74
 presupuesto_disponible = 10
 
 # COMPLETAR - INICIO
-
+presupuesto_disponible // precio
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -64,7 +65,7 @@ Restricción: Usar el operador módulo.
 numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
-
+numero_incalculable % es_divisible_por_siete
 # COMPLETAR - FIN
 
-assert es_divisible_por_siete
+assert es_divisible_por_siete == 7
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -10,7 +10,7 @@ esta_lloviendo = True
 riego_activado = True
 
 # COMPLETAR - INICIO
-
+piso_mojado = esta_lloviendo and riego_activado
 # COMPLETAR - FIN
 
 assert piso_mojado
@@ -26,7 +26,7 @@ lado_cuadrado = 5
 area_cuadrado = pow(lado_cuadrado, 2)
 
 # COMPLETAR - INICIO
-
+area_mayor_a_cinco = not(area_cuadrado < lado_cuadrado)
 # COMPLETAR - FIN
 
 assert area_mayor_a_cinco
@@ -41,7 +41,7 @@ numero_1 = 49
 numero_2 = 50
 
 # COMPLETAR - INICIO
-
+resultado = numero_1 % 7 and not numero_2 % 7
 # COMPLETAR - FIN
 
 assert resultado
@@ -60,7 +60,7 @@ variable_04 = "90"
 variable_05 = 100
 
 # COMPLETAR - INICIO
-
+resultado = (variable_01 or variable_02) or variable_03 / 0 or variable_04 / 0 or not variable_05 / 1
 # COMPLETAR - FIN
 
 assert resultado == 80
